b439a77feef580b0d4e3cf1eb191a129ad99949b
Daniel Nemeth <nemethdanka@gmail.com>
2020-04-25 16:18:59 +0200

    console input switch case

--- Prototype/src/App.java
+++ Prototype/src/App.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
 
 public class App {
+	private static Game game;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -10,6 +12,68 @@ public class App {
 		Flare flare = new Flare();
 		flare.use("AddPart");
 
+		processInput();
+
+	}
+
+	private static void processInput() {
+		Scanner lineScanner=new Scanner(System.in);
+		String line;
+		String[] words;
+		while(lineScanner.hasNextLine()){
+			line=lineScanner.nextLine();
+			words=line.split(" ");
+			if(words[0]!=null){
+				switch (words[0]){
+					case "newGame":
+						game=newGame();
+						break;
+					case "loadGame":
+						game=loadGame();
+						break;
+					case "saveGame":
+						saveGame();
+						break;
+					case "determinism":
+						setDeterminism(words);
+						break;
+					case "step":
+						step(words);
+						break;
+					case "eat":
+						eat(words);
+						break;
+					case "clearSnow":
+						clearSnow(words);
+						break;
+					case "excavate":
+						excavate();
+						break;
+					case "assembleGun":
+						assembleGun();
+						break;
+					case "inspect":
+						inspect(words);
+						break;
+					case "buildIgloo":
+						buildIgloo();
+						break;
+					case "setupTent":
+						setupTent(words);
+						break;
+					case "finish":
+						finish();
+						break;
+					case "storm":
+						storm(words);
+						break;
+					case "bear":
+						bear(words);
+						break;
+				}
+			}
+
+		}
 	}
 
 }
