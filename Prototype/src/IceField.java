c5eb45bbeb8851baaaff45e80324fa82b70b469e
Popovics Csaba <popovics.csaba96@gmail.com>
2020-04-26 08:06:09 +0200

    Játék logikája 90% kész

--- Prototype/src/IceField.java
+++ Prototype/src/IceField.java
@@ -1,4 +1,17 @@
 
 public class IceField extends Field{
+	protected Item item;
+	
+	public IceField() {super();}
+	
+	public IceField(String name) {super(name);}
+	
+	@Override
+	public Item getItem() { return item;}
+
+	@Override
+	public void removeItem() {
+		item = null;
+	}
 
 }
--- Prototype/src/UnstableIceField.java
+++ Prototype/src/UnstableIceField.java
@@ -1,4 +1,31 @@
+import java.util.Random;
 
 public class UnstableIceField extends IceField{
+	
+	public UnstableIceField() {
+		super();
+		limit = new Random().nextInt(Game.getCharacterCount()) + 1;
+	}
+	
+	public UnstableIceField(String name) {
+		super(name);
+		limit = new Random().nextInt(Game.getCharacterCount()) + 1;
+	}
+	
+	@Override
+	public void accept(Pawn p) {
+		if(p != null) {
+			characters.add(p);
+			p.setField(this);
+			if(characters.size() >= limit) {
+				flip();
+			}
+		}
+	}
+	
+	public void flip() {
+		for(Pawn pawn : characters)
+			pawn.fallIntoWater();
+	}
 
 }
